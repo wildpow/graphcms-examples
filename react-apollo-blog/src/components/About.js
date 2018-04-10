69ad6cc46ee8c34f7512f1d1f139006d37ad0423
Aaron Wilder <element1028@gmail.com>
2018-04-10 11:46:01 -0700

    about component updated working on post component

--- react-apollo-blog/src/components/About.js
+++ react-apollo-blog/src/components/About.js
@@ -1,32 +1,8 @@
 import React from 'react'
 import gql from 'graphql-tag'
-import { graphql } from 'react-apollo'
+import { Query } from 'react-apollo'
 
-const About = ({ data: { loading, error, allAuthors } }) => {
-  if (error) return <h1>Error fetching authors!</h1>
-  if (!loading) {
-    return (
-      <div>
-        {allAuthors.map(author => (
-          <div className='About-author' key={author.id}>
-            <div className='About-infoHeader'>
-              <img
-                className='About-img'
-                alt={author.name}
-                src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${author.avatar.handle}`}
-              />
-              <h1>Hello! My name is {author.name}</h1>
-            </div>
-            <p>{author.bibliography}</p>
-          </div>
-        ))}
-      </div>
-    )
-  }
-  return <h2>Loading author...</h2>
-}
-
-export const allAuthors = gql`
+const allAuthors = gql`
   query allAuthors {
     allAuthors {
       id
@@ -38,5 +14,32 @@ export const allAuthors = gql`
     }
   }
 `
+const About = () => (
+  <Query query={allAuthors}>
+    {({ loading, error, data }) => {
+      if (loading || !data) return <h2>Loading author...</h2>
+      if (error) return <h1>Error fetching authors!</h1>
+      
+      return (
+        <div>
+          {data.allAuthors.map(author => (
+            <div className='About-author' key={author.id}>
+              <div className='About-infoHeader'>
+                <img
+                  className='About-img'
+                  alt={author.name}
+                  src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${author.avatar.handle}`}
+                />
+                <h1>Hello! My name is {author.name}</h1>
+              </div>
+              <p>{author.bibliography}</p>
+            </div>
+          ))}
+      </div>
+      )
+    }}
+  </Query>
+)
 
-export default graphql(allAuthors)(About)
+export default About
+
--- react-apollo-blog/src/components/Post.js
+++ react-apollo-blog/src/components/Post.js
@@ -1,31 +1,31 @@
 import React from 'react'
 import gql from 'graphql-tag'
-import { graphql } from 'react-apollo'
+import { Query } from 'react-apollo'
 import Markdown from 'react-markdown'
 
-const Post = ({ data: { loading, error, post } }) => {
-  if (error) return <h1>Error fetching the post!</h1>
-  if (!loading) {
-    return (
-      <article>
-        <h1>{post.title}</h1>
-        <div className='Post-placeholder'>
-          <img
-            alt={post.title}
-            src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${post.coverImage.handle}`}
-          />
-        </div>
-        <Markdown
-          source={post.content}
-          escapeHtml={false}
-        />
-      </article>
-    )
-  }
-  return <h2>Loading post...</h2>
-}
+// const Post = ({ data: { loading, error, post } }) => {
+//   if (error) return <h1>Error fetching the post!</h1>
+//   if (!loading) {
+//     return (
+//       <article>
+//         <h1>{post.title}</h1>
+//         <div className='Post-placeholder'>
+//           <img
+//             alt={post.title}
+//             src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${post.coverImage.handle}`}
+//           />
+//         </div>
+//         <Markdown
+//           source={post.content}
+//           escapeHtml={false}
+//         />
+//       </article>
+//     )
+//   }
+//   return <h2>Loading post...</h2>
+// }
 
-export const singlePost = gql`
+const singlePost = gql`
   query singlePost($slug: String!) {
     post: Post(slug: $slug) {
       id
@@ -39,11 +39,36 @@ export const singlePost = gql`
     }
   }
 `
+const Post = ( slug ) => (
+  
+  <Query query={singlePost} variables={{slug: slug.match.params.slug }}>
+    {({ loading, error, post }) => { 
+      if (loading || !post) return <h2>Loading post...{console.log(post)}</h2>
+      if (error) return <h1>Error fetching the post!</h1>
 
-export default graphql(singlePost, {
-  options: ({ match }) => ({
-    variables: {
-      slug: match.params.slug
-    }
-  })
-})(Post)
+      return (
+        <article>
+        <h1>{post.title}</h1>
+        <div className='Post-placeholder'>
+          <img
+            alt={post.title}
+            src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${post.coverImage.handle}`}
+          />
+        </div>
+        <Markdown
+          source={post.content}
+          escapeHtml={false}
+        />
+      </article>
+      )
+    }}
+  </Query>
+)
+export default Post;
+// export default graphql(singlePost, {
+//   options: ({ match }) => ({
+//     variables: {
+//       slug: match.params.slug
+//     }
+//   })
+// })(Post)
